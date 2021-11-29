74288d76faf4ff26198ca70d035f3dec828b0f7f
ali-hassnain <alihassnain5@gmail.com>
2021-11-29 10:25:40 +0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '840cf678bdee332460458a45308614f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72ccd284-220f-4c87-bb54-e95d1a75d9b2'
+};
